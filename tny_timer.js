aadb1f4faed6087e4e34eb909a948c34f6717e39
Samuel <sharris163@west-mec.org>
2020-01-09 15:27:26 -0700

    step 5

---
+++ tny_timer.js
@@ -0,0 +1,26 @@
+"use strict";
+/*
+   New Perspectives on HTML5 and CSS3, 7th Edition
+   Tutorial 9
+   Review Assignment
+
+   Event Timer
+   Author: Samuel Harris
+   Date:  01/09/20 
+
+*/
+function showClock(){
+   var thisDay = new Date("May 19, 2018 9:31:27 a.m.");
+   var localDate = thisDay.toLocaleDateString();
+   var localTime = thisDay.toLocaleDateString();
+   document.getElementById("currentTime").innerHTML = "<span>localDate</span><span>localTime</span>";
+   
+}
+
+function nextJuly4(currentDate) {
+   var cYear = currentDate.getFullYear();
+   var jDate = new Date("July 4, 2018");
+   jDate.setFullYear(cYear);
+   if ((jDate - currentDate) < 0) jDate.setFullYear(cYear + 1);
+   return jDate;
+}
